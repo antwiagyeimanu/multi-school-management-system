ec09e53f0c980720d4db29275cd251eecdaf799e
Antwi-Agyei Manu <nana.antwi070@gmail.com>
2026-09-25 05:34:05 +0000

    Connect Django to PostgreSQL

--- sms/settings.py
+++ sms/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -74,10 +75,10 @@ TEMPLATES = [
 WSGI_APPLICATION = 'sms.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+    )
 }
 
 LANGUAGE_CODE = 'en-us'
